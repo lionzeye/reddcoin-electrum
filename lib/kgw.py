232fd39692e346f6f3c0b6b60cf037589b6dbe93
laudney <laudney@me.com>
2014-09-04 23:31:36 +0100

    Fix bugs in non-vectorised version of KGW

--- lib/kgw.py
+++ lib/kgw.py
@@ -9,7 +9,7 @@ class KGW(object):
     def __init__(self):
         self.target_block_spacing_seconds = 60
         self.time_day_seconds = 24 * 60 * 60
-        self.last_pow_block = 26799
+        self.last_pow_block = 260799
 
         # 0x00000FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF
         self.max_target = 2**236 - 1
@@ -36,8 +36,8 @@ class KGW(object):
 
         return past_blocks_min, past_blocks_max
 
-    def nbits(self, target):
-        # convert to bignum
+    def target2nbits(self, target):
+        target = int(target)
         MM = 256 * 256 * 256
         c = ("%064X" % target)[2:]
         i = 31
@@ -52,14 +52,23 @@ class KGW(object):
 
         return c + MM * i
 
+    def nbits2target(self, nbits):
+        nbits = int(nbits)
+        MM = 256 * 256 * 256
+        a = nbits % MM
+        if a < 0x8000:
+            a *= 256
+
+        return a * pow(2, 8 * (nbits / MM - 3))
+
     def get_target(self, chain=None):
         if chain is None or len(chain) == 0:
             return self.genesis_nbits, self.genesis_target
 
         first_block = chain[0]
         last_block = chain[-1]
-        last_timestamp = last_block.get('timestamp')
-        first_height = max(0, first_block.get('block_height')-1)
+        last_timestamp = int(last_block.get('timestamp'))
+        first_height = max(0, first_block.get('block_height') - 1)
         last_height = last_block.get('block_height')
         next_height = last_height + 1
 
@@ -87,8 +96,8 @@ class KGW(object):
 
             past_blocks_mass += 1
             block_reading = chain[-i]
-            timestamp_reading = block_reading.get('timestamp')
-            target_reading = block_reading.get('bits')
+            timestamp_reading = int(block_reading.get('timestamp'))
+            target_reading = self.nbits2target(block_reading.get('bits'))
 
             if i == 1:
                 past_target_average = float(target_reading)
@@ -124,8 +133,8 @@ class KGW(object):
             new_target *= past_rate_actual_seconds
             new_target /= past_rate_target_seconds
 
-        new_target = min(new_target, self.max_target)
-        new_nbits = self.nbits(new_target)
+        new_target = min(int(new_target), self.max_target)
+        new_nbits = self.target2nbits(new_target)
         return new_nbits, new_target
 
     def get_chain_target(self, prev_chain, chain):
