b334de4552077826167bbb1c93ed06b733eedf42
laudney <ren@reddcoin.com>
2014-09-15 20:38:20 +0100

    Update script for Wine.

--- contrib/build-wine/prepare-wine.sh
+++ contrib/build-wine/prepare-wine.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Please update these links carefully, some versions won't work under Wine
-PYTHON_URL=http://www.python.org/ftp/python/2.6.6/python-2.6.6.msi
-PYQT4_URL=http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.9.5/PyQt-Py2.6-x86-gpl-4.9.5-1.exe
-PYWIN32_URL=http://sourceforge.net/projects/pywin32/files/pywin32/Build%20218/pywin32-218.win32-py2.6.exe/download
-PYINSTALLER_URL=http://downloads.sourceforge.net/project/pyinstaller/2.0/pyinstaller-2.0.zip
+PYTHON_URL=http://www.python.org/ftp/python/2.7.8/python-2.7.8.msi
+PYQT4_URL=http://sourceforge.net/projects/pyqt/files/PyQt4/PyQt-4.11.2/PyQt4-4.11.2-gpl-Py2.7-Qt4.8.6-x32.exe
+PYWIN32_URL=http://sourceforge.net/projects/pywin32/files/pywin32/Build%20219/pywin32-219.win32-py2.7.exe/download
+PYINSTALLER_URL=https://pypi.python.org/packages/source/P/PyInstaller/PyInstaller-2.1.zip
 NSIS_URL=http://prdownloads.sourceforge.net/nsis/nsis-2.46-setup.exe?download
+SETUPTOOLS_URL=https://pypi.python.org/packages/2.7/s/setuptools/setuptools-0.6c11.win32-py2.7.exe
 #ZBAR_URL=http://sourceforge.net/projects/zbar/files/zbar/0.10/zbar-0.10-setup.exe/download
 
 # These settings probably don't need change
 export WINEPREFIX=/opt/wine-electrum
-PYHOME=c:/python26
+PYHOME=c:/python27
 PYTHON="wine $PYHOME/python.exe -OO -B"
 
 # Let's begin!
@@ -45,21 +46,24 @@ wine PyQt.exe
 # Install pyinstaller
 wget -O pyinstaller.zip "$PYINSTALLER_URL"
 unzip pyinstaller.zip
-mv pyinstaller-2.0 $WINEPREFIX/drive_c/pyinstaller
+mv PyInstaller-2.1 $WINEPREFIX/drive_c/pyinstaller
 
 # Patch pyinstaller's DummyZlib
-patch $WINEPREFIX/drive_c/pyinstaller/PyInstaller/loader/archive.py < ../archive.patch
+#patch $WINEPREFIX/drive_c/pyinstaller/PyInstaller/loader/archive.py < ../archive.patch
 
 # Install ZBar
 #wget -q -O zbar.exe "http://sourceforge.net/projects/zbar/files/zbar/0.10/zbar-0.10-setup.exe/download"
 #wine zbar.exe
 
+# Install setuptools
+wget -O setuptools.exe "$SETUPTOOLS_URL"
+wine setuptools.exe
+
 # Install dependencies
-wget -q -O - "http://python-distribute.org/distribute_setup.py" | $PYTHON
 wine "$PYHOME\\Scripts\\easy_install.exe" ecdsa slowaes #zbar
 
 # Install NSIS installer
-wget -q -O nsis.exe "http://prdownloads.sourceforge.net/nsis/nsis-2.46-setup.exe?download"
+wget -q -O nsis.exe "$NSIS_URL"
 wine nsis.exe
 
 # Install UPX
